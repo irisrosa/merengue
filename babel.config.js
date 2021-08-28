f97b992676576c1634b422d0957b35c9d2f61faa
Iris Rosa <3679686+irisrosa@users.noreply.github.com>
2021-08-29 00:00:09 +0200

    update babel preset typescript

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,9 @@
 module.exports = {
   presets: [
+    "@babel/preset-env",
     "@babel/preset-react",
     [
-      "@babel/typescript",
+      "@babel/preset-typescript",
       {
         allExtensions: true,
         isTSX: true,
