43bbc75b611195a079025e039f54b569249d5444
Iris Rosa <3679686+irisrosa@users.noreply.github.com>
2021-08-29 20:11:00 +0200

    fix blockset tests

--- src/__tests__/BlockSet.test.tsx
+++ src/__tests__/BlockSet.test.tsx
@@ -1,24 +1,15 @@
 import React from 'react';
-import { screen } from '@testing-library/react';
-import { lime } from '@src/theme/lime';
-import { BlockSet } from '@components/layout/FlexGrid/BlockSet';
-import { BlockData, BlockSetInterface } from '@components/layout/FlexGrid/types';
-import { ThemeContextRender } from '@src/testUtils/ContextRenderer';
+import { screen, render } from '@testing-library/react';
+import { BlockSet } from '@src/BlockSet';
+import { BlockData } from '@src/types';
 
 const noOpComp = () => <React.Fragment />;
 
 const blocks: BlockData[] = [{ style: 'dark', Component: noOpComp }];
 
-const renderComponent = (props: BlockSetInterface) => <BlockSet {...props} />;
-
 describe('FlexGrid BlockSet', () => {
   test('does not expands to the fullwidth', () => {
-    ThemeContextRender(
-      renderComponent({
-        blocks,
-      }),
-      'lime'
-    );
+    render(<BlockSet blocks={blocks} />);
 
     // Media queries are not registered as we can't properly simulate screen size
     expect(screen.getByTestId('blockset')).toHaveStyle({
@@ -28,52 +19,43 @@ describe('FlexGrid BlockSet', () => {
   });
 
   test('has a background image', () => {
-    ThemeContextRender(
-      renderComponent({
-        backgroundImage: {
+    render(
+      <BlockSet
+        blocks={blocks}
+        backgroundImage={{
           src: 'http://somepath/image.png',
-        },
-        blocks,
-      }),
-      'lime'
+        }}
+      />
     );
 
     expect(screen.queryAllByTestId('blockset-background-image')).toHaveLength(1);
   });
 
-  test('has dark background', () => {
-    ThemeContextRender(
-      renderComponent({ bleedBackground: true, blockSetStyle: 'dark', blocks }),
-      'lime'
+  test('bleed background', () => {
+    render(
+      <BlockSet
+        bleedBackground
+        blocks={blocks}
+        backgroundImage={{
+          src: 'http://somepath/image.png',
+        }}
+      />
     );
 
-    const el = screen.getByTestId('blockset');
-
-    expect(el).toHaveStyle({
-      backgroundColor: lime.colors.brand700,
-    });
-    expect(el).not.toHaveStyle({
+    expect(screen.getByTestId('blockset')).not.toHaveStyle({
       width: '100%',
       margin: '0px auto',
     });
   });
 
-  test('has default background', () => {
-    ThemeContextRender(renderComponent({ blocks }), 'lime');
-
-    expect(screen.getByTestId('blockset')).not.toHaveStyle({
-      backgroundColor: lime.colors.brand700,
-    });
-  });
-
   test('wraps blocks in container', () => {
-    ThemeContextRender(renderComponent({ blocks }), 'lime');
+    render(<BlockSet blocks={blocks} />);
 
     expect(screen.queryAllByTestId('flexgrid-container')).toHaveLength(1);
   });
 
   test('does not wrap blocks in container', () => {
-    ThemeContextRender(renderComponent({ blocks, bleedContent: true }), 'lime');
+    render(<BlockSet blocks={blocks} bleedContent />);
 
     expect(screen.queryAllByTestId('flexgrid-container')).toHaveLength(0);
   });
