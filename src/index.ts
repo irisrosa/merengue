429d95465d8de31954ea91f45c2ae6b1c4dbcff4
irisrosa <iris.rosa@gmail.com>
2023-02-06 19:57:33 +0100

    cleanup

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
+// export { FlexGrid } from './FlexGrid';
+// export type { GridData } from 'types';
 export { Block } from './Block';
-export { FlexGrid } from './FlexGrid';
-export type { GridData } from 'types';
