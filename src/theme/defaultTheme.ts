5da39d48e57aecf7e25208d0405230e6d073e185
irisrosa <iris.rosa@gmail.com>
2023-02-21 21:00:33 +0100

    remove prop from defaulttheme

--- src/theme/defaultTheme.ts
+++ src/theme/defaultTheme.ts
@@ -3,7 +3,6 @@ import { GridOptions } from '@src/types';
 export const theme: GridOptions = {
   blockPadding: '0px',
   gap: 1,
-  breakPoints: { small: 767, large: 992 },
   columns: 4,
   maxWidth: '960px',
 };
